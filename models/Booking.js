c3f44e6b691f40a177c807f0c889136ed6369328
Elish Neupane <elishneupane9@gmail.com>
2026-01-06 13:09:21 +0545

    Add files via upload

---
+++ models/Booking.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  subject: String,
+  message: String,
+  
+  }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
